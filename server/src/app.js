6dad6c6bd7ff48dea8061ac3568ce31991383015
Noriyuki Higashide <norishide@gmail.com>
2018-09-17 10:43:04 +0900

    setting up the registration page

--- server/src/app.js
+++ server/src/app.js
@@ -10,9 +10,15 @@ app.use(morgan('combine'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+// app.get('/status', (req, res) => {
+//   res.send({
+//     message: 'hello world!'
+//   })
+// })
+
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}! Your user was registered! Have Fun!`
   })
 })
 
